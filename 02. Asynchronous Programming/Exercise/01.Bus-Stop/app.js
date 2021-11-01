47a3ed8eec70e5ad2b11c08d2cb980bdb6c3149a
Zdravko Zdravkov <pirocorp@abv.bg>
2021-11-01 20:00:47 +0100

    01.Bus-Stop updated

--- 02. Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ 02. Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,4 +1,36 @@
-function getInfo() {
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    const stopNameElement = document.getElementById('stopName');
+    const busesElement = document.getElementById('buses');
+
+    try {
+        stopNameElement.textContent = 'Loading...'
+        busesElement.replaceChildren();
+        const res = await fetch(url);
+
+        if(res.status != 200) {
+            throw new Error('Stop Id not found');
+        }
+
+        const data = await res.json();
+        
+        stopNameElement.textContent = data.name;
+
+        for (const bus in data.buses) {
+            const busElement = document.createElement('li');
+            busElement.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
+
+            busesElement.appendChild(busElement);
+        }
+    } catch(err) {
+        stopNameElement.textContent = 'Error';        
+    }
+}
+
+function promiseSolution() {
     const stopId = document.getElementById('stopId').value;
 
     const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
@@ -22,5 +54,5 @@ function getInfo() {
         .catch(err => {
             stopNameElement.textContent = 'Error';
             busesElement.innerHTML = '';
-        });    
+        });
 }
